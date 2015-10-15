8a484254b54d27a0d9d16f15a681ba70da2b1584
yoorek <yoorek@gmail.com>
2015-10-15 11:03:42 +0200

    refactor: some

--- src/modules/menu/class/MenuController.ts
+++ src/modules/menu/class/MenuController.ts
@@ -1,74 +1,21 @@
+import {sections} from './Menu';
+
 /*@ngInject*/
 class MenuController {
 
     private openedSection: any;
     private toggleDisabled;
-
-    private sections = [{
-        name: 'Customization',
-        type: 'heading',
-        children: [
-            {
-                name: 'CSS',
-                type: 'toggle',
-                icon: 'home',
-                pages: [{
-                    name: 'Typography',
-                    url: '/CSS/typography',
-                    type: 'link'
-                },
-                    {
-                        name: 'Button',
-                        url: '/CSS/button',
-                        type: 'link'
-                    },
-                    {
-                        name: 'Checkbox',
-                        url: '/CSS/checkbox',
-                        type: 'link'
-                    }]
-            },
-            {
-                name: 'Theming',
-                type: 'toggle',
-                icon: 'favorite_border',
-                pages: [
-                    {
-                        name: 'Configuring a Theme',
-                        url: '/Theming/03_configuring_a_theme',
-                        type: 'link'
-                    },
-                    {
-                        name: 'Multiple Themes',
-                        url: '/Theming/04_multiple_themes',
-                        type: 'link'
-                    }
-                ]
-            }
-        ]
-    }];
-
-    private notifications = {
-        count: 13,
-        items: [
-            { date: '12.05.2015 13:43', title: 'Title for notifications quite long so should be ellipsed', text: 'Text for notifications quite long so should be ellipsed', icon: 'alarm', type: 'warning', id: 1 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'notifications_none', type: 'info', id: 2 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'error_outline', type: 'error', id: 3 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications quite long so should be ellipsed', text: 'Text for notifications quite long so should be ellipsed', icon: 'alarm', type: 'warning', id: 1 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'notifications_none', type: 'info', id: 2 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'error_outline', type: 'error', id: 3 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications quite long so should be ellipsed', text: 'Text for notifications quite long so should be ellipsed', icon: 'alarm', type: 'warning', id: 1 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'notifications_none', type: 'info', id: 2 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'error_outline', type: 'error', id: 3 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications quite long so should be ellipsed', text: 'Text for notifications quite long so should be ellipsed', icon: 'alarm', type: 'warning', id: 1 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'notifications_none', type: 'info', id: 2 },
-            { date: '12.05.2015 13:43', title: 'Title for notifications', text: 'Text for notifications', icon: 'error_outline', type: 'error', id: 3 },
-        ]
-    };
+    private notifications;
+    private sections = sections;
 
     constructor(
-        private $mdSidenav: angular.material.ISidenavService) {
+        private $mdSidenav: angular.material.ISidenavService,
+        private $http: angular.IHttpService) {
 
+        $http.get('data/notifications.json')
+            .then((response) => {
+                this.notifications = response.data;
+            });
     }
 
     toggleLeftSidebar() {
