3bfda5fa5eb320a7854fa52de8a82e0bab34f747
yoorek <yoorek@gmail.com>
2015-12-26 10:49:53 +0100

    chore: working JSPM version (with huge dependencies problem)

--- bin/link.sh
+++ bin/link.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rm /Users/yoorek/.jspm/linked/github/solubis/angular-ui@master
+rm /Users/yoorek/.jspm/linked/github/solubis/angular-components@master
+rm /Users/yoorek/.jspm/linked/github/solubis/angular-datatable@master
+
 ln -sFf /Users/yoorek/Work/angular-console/ui /Users/yoorek/.jspm/linked/github/solubis/angular-ui@master
 ln -sFf /Users/yoorek/Work/angular-components /Users/yoorek/.jspm/linked/github/solubis/angular-components@master
 ln -sFf /Users/yoorek/Work/angular-datatable /Users/yoorek/.jspm/linked/github/solubis/angular-datatable@master
